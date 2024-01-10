ceb72f1544bb235e527e93d879a16b7a17934299
Jayanth <jayanth.sms.in@gmail.com>
2024-01-10 23:16:29 +0530

    NFT contract

---
+++ contracts/CryptoDevsNFT.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+
+contract CryptoDevsNFT is ERC721Enumerable {
+    
+constructor() ERC721("CryptoDevsNFT", "CD") {}
+
+function mint() public{
+    _safeMint(msg.sender, totalSupply());
+}
+
+}
